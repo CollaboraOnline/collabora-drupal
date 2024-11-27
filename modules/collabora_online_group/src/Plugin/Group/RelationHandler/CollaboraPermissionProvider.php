a1b9547b5a677d91e4839fa21d58a81de5a79afc
Andreas Hennings <andreas@dqxtech.net>
2024-11-27 17:39:10 +0100

    Add copyright notice in php files.

--- modules/collabora_online_group/src/Plugin/Group/RelationHandler/CollaboraPermissionProvider.php
+++ modules/collabora_online_group/src/Plugin/Group/RelationHandler/CollaboraPermissionProvider.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 declare(strict_types=1);
 
 namespace Drupal\collabora_online_group\Plugin\Group\RelationHandler;
