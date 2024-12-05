dba5504b44f6372139da373c65d48bdff0a1eaa9
Andreas Hennings <andreas@dqxtech.net>
2024-12-05 17:07:09 +0100

    Issue 69: Use MediaInterface as parameter type, instead of Media, in MediaHelper.

--- src/MediaHelper.php
+++ src/MediaHelper.php
@@ -16,7 +16,7 @@ namespace Drupal\collabora_online;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\file\Entity\File;
-use Drupal\media\Entity\Media;
+use Drupal\media\MediaInterface;
 
 /**
  * Service to assist with media entities.
@@ -36,13 +36,13 @@ class MediaHelper {
   /**
    * Gets the file referenced by a media entity.
    *
-   * @param \Drupal\media\Entity\Media $media
+   * @param \Drupal\media\MediaInterface $media
    *   The media entity.
    *
    * @return \Drupal\file\FileInterface|null
    *   The file entity, or NULL if not found.
    */
-  public function getFile(Media $media) {
+  public function getFile(MediaInterface $media) {
     $fid = $media->getSource()->getSourceFieldValue($media);
     /** @var \Drupal\file\FileInterface|null $file */
     $file = $this->entityTypeManager->getStorage('file')->load($fid);
@@ -68,12 +68,12 @@ class MediaHelper {
   /**
    * Sets the file entity reference for a media entity.
    *
-   * @param \Drupal\media\Entity\Media $media
+   * @param \Drupal\media\MediaInterface $media
    *   Media entity to be modified.
    * @param \Drupal\file\Entity\File $source
    *   File entity to reference.
    */
-  public function setMediaSource(Media $media, File $source) {
+  public function setMediaSource(MediaInterface $media, File $source) {
     $name = $media->getSource()->getSourceFieldDefinition($media->bundle->entity)->getName();
     $media->set($name, $source);
   }
