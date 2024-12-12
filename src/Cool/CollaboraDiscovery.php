9125d29c33f057b41b47bb53390088b03b22466f
Andreas Hennings <andreas@dqxtech.net>
2024-12-13 00:04:42 +0100

    Issue 75: Intercept errors from XML parsing.

--- src/Cool/CollaboraDiscovery.php
+++ src/Cool/CollaboraDiscovery.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 namespace Drupal\collabora_online\Cool;
 
 use Drupal\collabora_online\Exception\CollaboraNotAvailableException;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 /**
  * Service to get a WOPI client URL for a given MIME type.
@@ -72,9 +73,23 @@ class CollaboraDiscovery implements CollaboraDiscoveryInterface {
    *   The XML is invalid or empty.
    */
   protected function parseXml(string $xml): \SimpleXMLElement {
-    $parsed_xml = simplexml_load_string($xml);
-    if (!$parsed_xml) {
-      throw new CollaboraNotAvailableException('The retrieved discovery.xml file is not a valid XML file.');
+    try {
+      // Avoid errors from XML parsing hitting the regular error handler.
+      // An alternative would be libxml_use_internal_errors(), but then we would
+      // have to deal with the results from libxml_get_errors().
+      $parsed_xml = ErrorHandler::call(
+        fn () => simplexml_load_string($xml),
+      );
+    }
+    catch (\ErrorException $e) {
+      throw new CollaboraNotAvailableException('Error in the retrieved discovery.xml file: ' . $e->getMessage(), previous: $e);
+    }
+    if ($parsed_xml === FALSE) {
+      // The parser returned FALSE, but no error was raised.
+      // This is known to happen when $xml is an empty string.
+      // Instead we could check for $xml === '' earlier, but we don't know for
+      // sure if this is, and always will be, the only such case.
+      throw new CollaboraNotAvailableException('The discovery.xml file is empty.');
     }
     return $parsed_xml;
   }
