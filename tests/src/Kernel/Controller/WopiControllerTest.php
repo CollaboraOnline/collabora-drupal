df813c7a31c5c8fdcd0a7f2ad429e3d2e84f2319
AaronGilMartinez <aaron.gil-martinez@ext.ec.europa.eu>
2025-01-20 12:51:24 +0100

    Fix test.

--- tests/src/Kernel/Controller/WopiControllerTest.php
+++ tests/src/Kernel/Controller/WopiControllerTest.php
@@ -349,7 +349,7 @@ New file: @new_file_id / @new_file_uri',
         RfcLogLevel::WARNING,
         'A token cannot be decoded: @message',
         [
-          '@message' => 'No key was chosen for use in Collabora.',
+          '@message' => 'The key with id \'_unknown_key_\' is empty or does not exist.',
         ],
         assertion_message: $name,
       );
