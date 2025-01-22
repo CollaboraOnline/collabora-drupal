752495eb086da202fe6f4d40839590bb7594dfc8
Andreas Hennings <andreas@dqxtech.net>
2025-01-22 17:49:33 +0100

    Config URLs: Let the protected method return the complete discovery URL in CollaboraDiscoveryFetcher.

--- src/Cool/CollaboraDiscoveryFetcher.php
+++ src/Cool/CollaboraDiscoveryFetcher.php
@@ -48,7 +48,7 @@ class CollaboraDiscoveryFetcher implements CollaboraDiscoveryFetcherInterface {
    * {@inheritdoc}
    */
   public function getDiscoveryXml(): string {
-    $discovery_url = $this->getWopiClientServerBaseUrl() . '/hosting/discovery';
+    $discovery_url = $this->getDiscoveryUrl();
 
     $cool_settings = $this->loadSettings();
     $disable_checks = !empty($cool_settings['disable_cert_check']);
@@ -75,16 +75,15 @@ class CollaboraDiscoveryFetcher implements CollaboraDiscoveryFetcherInterface {
   }
 
   /**
-   * Loads the WOPI server url from configuration.
+   * Gets the URL to fetch the discovery.
    *
    * @return string
-   *   Base URL to access the WOPI server from Drupal.
+   *   URL to fetch the discovery XML.
    *
    * @throws \Drupal\collabora_online\Exception\CollaboraNotAvailableException
-   *   The WOPI server url is misconfigured, or the protocol does not match
-   *   that of the current Drupal request.
+   *   The WOPI server url is misconfigured.
    */
-  protected function getWopiClientServerBaseUrl(): string {
+  protected function getDiscoveryUrl(): string {
     $cool_settings = $this->loadSettings();
     $wopi_client_server = $cool_settings['server'] ?? NULL;
     if (!$wopi_client_server) {
@@ -99,7 +98,7 @@ class CollaboraDiscoveryFetcher implements CollaboraDiscoveryFetcherInterface {
       ));
     }
 
-    return $wopi_client_server;
+    return $wopi_client_server . '/hosting/discovery';
   }
 
   /**
