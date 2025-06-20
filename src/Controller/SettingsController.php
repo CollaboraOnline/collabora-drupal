d027d43a213ca7985412c8e19bf083a6ee68dd2c
Andreas Hennings <andreas@dqxtech.net>
2025-06-21 01:34:23 +0200

    Introduce settings iframe route.

---
+++ src/Controller/SettingsController.php
@@ -0,0 +1,181 @@
+<?php
+
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+declare(strict_types=1);
+
+namespace Drupal\collabora_online\Controller;
+
+use Drupal\collabora_online\Discovery\DiscoveryFetcherInterface;
+use Drupal\collabora_online\Exception\CollaboraNotAvailableException;
+use Drupal\collabora_online\Jwt\JwtTranscoderInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\Core\Utility\Error;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Provides route responses for the Collabora settings iframe.
+ */
+class SettingsController implements ContainerInjectionInterface {
+
+  use AutowireTrait;
+  use StringTranslationTrait;
+
+  public function __construct(
+    protected readonly DiscoveryFetcherInterface $discoveryFetcher,
+    protected readonly JwtTranscoderInterface $jwtTranscoder,
+    protected readonly RendererInterface $renderer,
+    #[Autowire('logger.channel.collabora_online')]
+    protected readonly LoggerInterface $logger,
+    protected readonly ConfigFactoryInterface $configFactory,
+    protected readonly AccountInterface $currentUser,
+    TranslationInterface $string_translation,
+  ) {
+    $this->setStringTranslation($string_translation);
+  }
+
+  /**
+   * Returns a raw page with an embedded settings iframe.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The response.
+   */
+  public function settings(Request $request): Response {
+    try {
+      // @todo Get client url for the correct MIME type.
+      $discovery = $this->discoveryFetcher->getDiscovery();
+      $settings_url = $discovery->getSettingsIframeURL();
+    }
+    catch (CollaboraNotAvailableException $e) {
+      $this->logger->warning(
+        "Collabora Online is not available.<br>\n" . Error::DEFAULT_ERROR_MESSAGE,
+        Error::decodeException($e) + [],
+      );
+      return new Response(
+        (string) $this->t('Collabora Online is not available.'),
+        Response::HTTP_BAD_REQUEST,
+        ['content-type' => 'text/plain'],
+      );
+    }
+    if ($settings_url === NULL) {
+      return new Response(
+        (string) $this->t('The Collabora Online settings page is not available.'),
+        Response::HTTP_BAD_REQUEST,
+        ['content-type' => 'text/plain'],
+      );
+    }
+
+    $current_request_scheme = $request->getScheme();
+    if (parse_url($settings_url, PHP_URL_SCHEME) !== $current_request_scheme) {
+      $this->logger->error(
+        "The current request uses '@current_request_scheme' url scheme, but the Collabora settings iframe url is '@wopi_client_url'.",
+        [
+          '@current_request_scheme' => $current_request_scheme,
+          '@wopi_client_url' => $settings_url,
+        ],
+      );
+      return new Response(
+        (string) $this->t('Viewer error: Protocol mismatch.'),
+        Response::HTTP_BAD_REQUEST,
+        ['content-type' => 'text/plain'],
+      );
+    }
+
+    try {
+      $render_array = $this->build($settings_url);
+    }
+    catch (CollaboraNotAvailableException $e) {
+      $this->logger->warning(
+        "Cannot show the viewer/editor.<br>\n" . Error::DEFAULT_ERROR_MESSAGE,
+        Error::decodeException($e) + [],
+      );
+      return new Response(
+        (string) $this->t('The Collabora Online editor/viewer is not available.'),
+        Response::HTTP_BAD_REQUEST,
+        ['content-type' => 'text/plain'],
+      );
+    }
+
+    return new Response((string) $this->renderer->renderRoot($render_array));
+  }
+
+  /**
+   * Gets a render array for a cool viewer.
+   *
+   * @param string $settings_url
+   *   The WOPI client url.
+   *
+   * @return array
+   *   A stub render element.
+   *
+   * @throws \Drupal\collabora_online\Exception\CollaboraNotAvailableException
+   *   The key to use by Collabora is empty or not configured.
+   */
+  protected function build(string $settings_url): array {
+    $config = $this->configFactory->get('collabora_online.settings');
+
+    /** @var string|null $wopi_base */
+    $wopi_base = $config->get('cool.wopi_base');
+    if (!$wopi_base) {
+      throw new CollaboraNotAvailableException('The Collabora Online connection is not configured.');
+    }
+
+    // A trailing slash is optional in the configured URL.
+    $wopi_base = rtrim($wopi_base, '/');
+    $expire_timestamp = $this->getExpireTimestamp();
+    $access_token = $this->jwtTranscoder->encode(
+      [
+        'uid' => $this->currentUser->id(),
+      ],
+      $expire_timestamp,
+    );
+
+    return [
+      '#theme' => 'collabora_online_settings_iframe',
+      '#formActionUrl' => $settings_url,
+      '#postData' => [
+        'access_token' => $access_token,
+        'access_token_ttl' => $expire_timestamp * 1000,
+        'wopi_setting_base_url' => $wopi_base . '/cool/wopi/settings',
+        'iframe_type' => 'admin',
+      ],
+    ];
+  }
+
+  /**
+   * Gets a token expiration timestamp based on the configured TTL.
+   *
+   * @return float
+   *   Expiration timestamp in seconds, with millisecond accuracy.
+   */
+  protected function getExpireTimestamp(): float {
+    /** @var int $ttl_seconds */
+    $ttl_seconds = $this->configFactory->get('collabora_online.settings')
+      ->get('cool.access_token_ttl') ?? 0;
+    // Set a fallback of 24 hours.
+    $ttl_seconds = $ttl_seconds ?: 86400;
+
+    return gettimeofday(TRUE) + $ttl_seconds;
+  }
+
+}
